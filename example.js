5dc8180c36689a1b91bae8e413180c82b0843e1b
Ddo <joeddo89@gmail.com>
2014-07-02 15:09:54 +0700

    init

--- example.js
+++ example.js
@@ -1,26 +1,6 @@
-node-resemble
-=============
-
-Node port of [Resemble.js](https://github.com/Huddle/Resemble.js).
-
-##Installation
-
-```bash
-$ npm i node-resemble
-```
-
-##Note
-
-[node-canvas](https://github.com/Automattic/node-canvas)  is a [Cairo](http://cairographics.org/) backed Canvas implementation for NodeJS.
-
-Unless previously installed you'll need Cairo. For system-specific installation view the node-canvas [Wiki](https://github.com/Automattic/node-canvas/wiki/_pages).
-
-##Example
-
-```js
 var fs = require('fs');
 
-var resemble = require('node-resemble');
+var resemble = require('./');
 
 var github_img1 = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAABGdBTUEAALGPC/xhBQAAAJlQTFRFJiYmJiYmJSUlJiYmKysrJiYmJSUlJiYmJSUlJCQkJiYmJycnJiYmJiYmJSUlJiYmJSUlJiYmJSUlJCQkJycnJCQkJycnJiYmJSUlJiYmJiYmJycnJiYmJiYmJSUlJycnJycnJCQkJSUlJiYmJiYmJycnJiYmJycnJiYmJSUlJiYmJiYmJycnJiYmEhISISEhJycnAAAAJiYmQmWwmAAAADJ0Uk5T9JNotgyiTK2KR0P7Zij+PYni/StBMdmBpKnq5tbw1VXsKu9RUM3O7cl8V5ql3w42+gB0ejcrAAAApElEQVQY012PRRIEMQwDs8zMOAzhyP9/3CYDl9XJ7irZEoOXldo5LW2YGWAUlVwIXpIyAZi6ytAor2rjgao2pgXjpFJglvLLdrbez4eT1TIny2QJS50WKCXTHLceDMA1cwLXHsQQjrkIhx4wRC5YRj2og0UW2Ll2P05RSP82g72/iJ6PMzL/FipOgIJIA6cQzEdPBT7eAZE20UO5b7jy7sr91f8BZR4qkBlEJUYAAAAASUVORK5CYII=';
 
@@ -33,4 +13,3 @@ resemble(github_img1).onComplete(function(data){
 resemble(github_img1).compareTo(github_img2).onComplete(function(data){
     console.log(data);
 });
-```
---
+++ index.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+
+var resemblejs = require('resemblejs');
+var canvas     = require('canvas');
+var imageType  = require('image-type');
+
+module.exports = function() {
+    //override FileReader
+    this.FileReader = function(){};
+    this.FileReader.prototype.readAsDataURL = function(buffer) {
+        this.onload({
+            target: {
+                result: 'data:image/' + imageType(buffer) + ';base64,' + buffer.toString('base64')
+            }
+        });
+    };
+
+    //override Image
+    this.Image = canvas.Image;
+    this.Image.prototype.setAttribute = function() {};
+
+    //override document.createElement('canvas')
+    this.document = {
+        createElement: function(tag) {
+            if(tag !== 'canvas') {
+                return false;
+            }
+
+            return new canvas;
+        }
+    };
+
+    return resemblejs.resemble.apply(this, arguments);
+};
